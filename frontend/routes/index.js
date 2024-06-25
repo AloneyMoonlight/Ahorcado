377b7254771bd73bbe50dd173bddb0db431ff901
JosueAndresArayaPereira <145078529+JosueAndresArayaPereira@users.noreply.github.com>
2024-06-25 18:10:33 -0400

    creacion base de el servidor

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,4 +2,12 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Express" });
+});
+
+indexRouter.get("/*", (req, res) => {
+  res.render("404", { title: "404" });
+});
+
 export default indexRouter;
